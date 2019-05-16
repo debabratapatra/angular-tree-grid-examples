2de605110bf1694646721a66c70ab94e175f9393
debabratapatra <debabratapatra12@gmail.com>
2019-05-16 15:06:36 +0530

    Apply menu selection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { debug } from 'util';
 
 @Component({
   selector: 'my-app',
@@ -8,4 +9,23 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
   
+  selected_feature = {
+    basic_tree_grid: true,
+    add_edit_delete: false,
+    cond_row_edit: false,
+    custom_view_component: false,
+    custom_edit_component: false,
+    resolve_row_add: false,
+    filter_column: false,
+  }
+
+  select(feature) {
+    for (const key in this.selected_feature) {
+      if (this.selected_feature.hasOwnProperty(key)) {        
+        this.selected_feature[key] = false;
+      }
+    }
+
+    this.selected_feature[feature] = true;
+  }
 }
