941772e8377c8c660ef323c20dee40e83ed5cd54
debabratapatra <debabratapatra12@gmail.com>
2022-01-19 15:43:01 +0530

    Make Responsive.

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,31 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
+
+function updateCodeWidth() {
+  var windowWidth = window.screen.availWidth;
+  if(windowWidth > 1100) {
+    return;
+  }
+  var offsetWidth = (<HTMLElement>document.querySelector('td.left-panel')).offsetWidth;
+  var contentWidth = windowWidth - offsetWidth;
+  var padding = 20;
+  var extra = 5;
+
+  if(windowWidth > 768) {
+    padding = 60;
+    extra = 40;
+  }
+
+  contentWidth = contentWidth - padding - extra;
+  var codes = document.querySelectorAll('div.code');
+
+  codes.forEach(code => {
+    (<HTMLElement>code).style.width = contentWidth + 'px';
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  updateCodeWidth();
+  window.addEventListener('resize', updateCodeWidth);
+});
